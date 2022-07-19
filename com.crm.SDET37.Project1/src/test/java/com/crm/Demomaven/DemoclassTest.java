729760acd6575dfb673e7fc90ad6c9df8686e801
INDRAJIT <INDRAJIT@INDRAJIT>
2022-07-19 12:08:32 +0530

    change

--- com.crm.SDET37.Project1/src/test/java/com/crm/Demomaven/DemoclassTest.java
+++ com.crm.SDET37.Project1/src/test/java/com/crm/Demomaven/DemoclassTest.java
@@ -1,5 +1,18 @@
 package com.crm.Demomaven;
 
+import org.testng.annotations.Test;
+
 public class DemoclassTest {
 
+	@Test
+	public void Demo()
+	{
+		System.out.println("hello");
+	}
+	@Test
+	public void Test()
+	{
+		System.out.println("world");
+	}
+	
 }
